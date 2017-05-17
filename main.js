5b35cc966369e15b1a0f9821c8857f93edcbdd55
ling zhou <lingzhoudesign@gmail.com>
2017-05-17 10:59:25 -0400

    first commit
    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+
+
+// Changing the defaults
+window.sr = ScrollReveal({ scale: 0.9, easing: 'cubic-bezier(0.6, 0.2, 0.1, 1)', reset: true });
+
+// Customizing a reveal set
+sr.reveal('.tile', { duration: 500 });
