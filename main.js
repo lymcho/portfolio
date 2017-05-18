25be3f0724678eecc78099769d5efefcc3968ebb
ling zhou <lingzhoudesign@gmail.com>
2017-05-18 12:12:58 -0400

    changes

--- main.js
+++ main.js
@@ -5,3 +5,7 @@ window.sr = ScrollReveal({ scale: 0.9, easing: 'cubic-bezier(0.6, 0.2, 0.1, 1)',
 
 // Customizing a reveal set
 sr.reveal('.tile', { duration: 500 });
+
+
+//smoothscroll initializing settings
+smoothScroll.init({speed:600,});
