80913024edbe43d64bc4916718091218beee6d1e
Ling Zhou <lingzhoudesign@gmail.com>
2021-10-07 18:00:32 -0500

    update
    update

---
+++ javascript/pw.js
@@ -0,0 +1,43 @@
+
+function openProject_illo() {
+    var testV = 1;
+    var pass1 = prompt('Please Enter Password',' ');
+    while (testV < 3) {
+    if (!pass1)
+    history.go(-1);
+    if (pass1.toLowerCase() == "letmein") {
+    alert('You Got it Right!');
+    window.location.href="html/g_illo.html"
+    break;
+    }
+    testV+=1;
+    var pass1 =
+    prompt('Access Denied - Password Incorrect, Please Try Again.','Password');
+    }
+    if (pass1.toLowerCase()!="password" & testV ==3)
+    history.go(-1);
+    return " ";
+}
+    
+ 
+    function openProject_booking() {
+        var testV = 1;
+        var pass1 = prompt('Please Enter Password',' ');
+        while (testV < 3) {
+        if (!pass1)
+        history.go(-1);
+        if (pass1.toLowerCase() == "letmein") {
+        alert('You Got it Right!');
+        window.location.href="html/g_booking.html"
+        break;
+        }
+        testV+=1;
+        var pass1 =
+        prompt('Access Denied - Password Incorrect, Please Try Again.','Password');
+        }
+        if (pass1.toLowerCase()!="password" & testV ==3)
+        history.go(-1);
+        return " ";
+    }
+        
+      
