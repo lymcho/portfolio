775a33b25f0bcf43a0d8f1199cb4a7aa8370ef5e
Ling Zhou <lingzhoudesign@gmail.com>
2021-10-08 15:14:30 -0500

    removing some projects
    update

--- javascript/pw.js
+++ javascript/pw.js
@@ -1,4 +1,4 @@
-
+//these are for password-protected projects
 function openProject_illo() {
     var testV = 1;
     var pass1 = prompt('Please Enter Password',' ');
@@ -6,7 +6,6 @@ function openProject_illo() {
     if (!pass1)
     history.go(-1);
     if (pass1.toLowerCase() == "letmein") {
-    alert('You Got it Right!');
     window.location.href="html/g_illo.html"
     break;
     }
@@ -20,24 +19,23 @@ function openProject_illo() {
 }
     
  
-    function openProject_booking() {
-        var testV = 1;
-        var pass1 = prompt('Please Enter Password',' ');
-        while (testV < 3) {
-        if (!pass1)
-        history.go(-1);
-        if (pass1.toLowerCase() == "letmein") {
-        alert('You Got it Right!');
-        window.location.href="html/g_booking.html"
-        break;
-        }
-        testV+=1;
-        var pass1 =
-        prompt('Access Denied - Password Incorrect, Please Try Again.','Password');
-        }
-        if (pass1.toLowerCase()!="password" & testV ==3)
-        history.go(-1);
-        return " ";
+function openProject_booking() {
+    var testV = 1;
+    var pass1 = prompt('Please Enter Password',' ');
+    while (testV < 3) {
+    if (!pass1)
+    history.go(-1);
+    if (pass1.toLowerCase() == "letmein") {
+    window.location.href="html/g_booking.html"
+    break;
     }
+    testV+=1;
+    var pass1 =
+    prompt('Access Denied - Password Incorrect, Please Try Again.','Password');
+    }
+    if (pass1.toLowerCase()!="password" & testV ==3)
+    history.go(-1);
+    return " ";
+}
         
       
