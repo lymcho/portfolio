79508f709488cc390b5bb120991abce6f7a3b1e2
Ling Zhou <lingzhoudesign@gmail.com>
2021-10-09 15:40:09 -0500

    pw update
    pw update so public can't see it yet

--- javascript/pw.js
+++ javascript/pw.js
@@ -1,4 +1,24 @@
 //these are for password-protected projects
+function openProject_booking() {
+    var testV = 1;
+    var pass1 = prompt('Please Enter Password',' ');
+    while (testV < 3) {
+    if (!pass1)
+    window.location.href="index.html"
+    if (pass1.toLowerCase() == "Ling") {
+    window.location.href="html/g_cs.html"
+    break;
+    }
+    testV+=1;
+    var pass1 =
+    prompt('Access Denied - Password Incorrect, Please Try Again.','Password');
+    }
+    if (pass1.toLowerCase()!="password" & testV ==3)
+    window.location.href="index.html"
+    return " ";
+}
+        
+  
 function openProject_illo() {
     var testV = 1;
     var pass1 = prompt('Please Enter Password',' ');
@@ -6,7 +26,7 @@ function openProject_illo() {
     if (!pass1)
     window.location.href="index.html"
     // history.go(-1);
-    if (pass1.toLowerCase() == "letmein") {
+    if (pass1.toLowerCase() == "Ling") {
     window.location.href="html/g_illo.html"
     break;
     }
@@ -27,7 +47,7 @@ function openProject_booking() {
     while (testV < 3) {
     if (!pass1)
     window.location.href="index.html"
-    if (pass1.toLowerCase() == "letmein") {
+    if (pass1.toLowerCase() == "Ling") {
     window.location.href="html/g_booking.html"
     break;
     }
